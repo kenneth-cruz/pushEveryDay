f852ef1bf79e76c36f8c475f9fb1e5bfb9db17e2
kenneth-cruz <54089933+kenneth-cruz@users.noreply.github.com>
2020-08-27 19:59:46 -0400

    adding day 17

---
+++ Take2/Day_017_mathops.js
@@ -0,0 +1,15 @@
+function basicOp(operation, value1, value2)
+{
+  if(operation === '+'){
+    return value1 + value2
+  }
+  if(operation === '-'){
+    return value1 - value2
+  }
+  if(operation === '*'){
+    return value1 * value2
+  }
+  if(operation === '/'){
+    return value1 / value2
+  }
+}
