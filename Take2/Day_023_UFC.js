7aa92d3bde3e0b0a0c35a46e6fb86b801fde4558
kenneth-cruz <54089933+kenneth-cruz@users.noreply.github.com>
2020-09-04 18:08:26 -0400

    add day 23

---
+++ Take2/Day_023_UFC.js
@@ -0,0 +1,3 @@
+var quote = function(fighter) {
+    return (fighter.toLowerCase() === 'george saint pierre' ? 'I am not impressed by your performance.' : `I'd like to take this chance to apologize.. To absolutely NOBODY!` )
+  };
