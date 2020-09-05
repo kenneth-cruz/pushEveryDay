a5c9f72a61a26eeb964271097093deac3d86e336
kenneth-cruz <54089933+kenneth-cruz@users.noreply.github.com>
2020-09-05 14:56:54 -0400

    day 24

--- Take2/Day_024_grades.js
+++ Take2/Day_024_grades.js
@@ -1,5 +1,5 @@
 function finalGrade (exam, projects) {
-    return (exam > 90 || projects > 10 ? 100 :
-           (exam > 75 && projects >= 5 ? 90  :
-           (exam > 50 && projects >= 2 ? 75 : 0)))
-  }
+  return (exam > 90 || projects > 10 ? 100 :
+         (exam > 75 && projects >= 5 ? 90  :
+         (exam > 50 && projects >= 2 ? 75 : 0)))
+}
