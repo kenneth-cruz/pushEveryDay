cb22a35460af1e1ac3a8120c758164c39795d9f1
kenneth-cruz <54089933+kenneth-cruz@users.noreply.github.com>
2020-10-26 17:03:28 -0400

    day 74

---
+++ Take2/Day_074_prices.js
@@ -0,0 +1,12 @@
+var finalPrices = function(prices) {
+    let answerArray = []
+    for(let i = 0; i < prices.length-1; i++){
+        for (let j = i+1; j < prices.length; j++) {
+            if (prices[i] >= prices[j]) {
+            prices[i] -= prices[j];
+            break;
+            }
+        }
+    }
+    return prices
+};
