574877f8978e4f85575d9eeff4999af87930552c
kenneth-cruz <kenneth.s.cruz@gmail.com>
2020-11-22 07:25:58 -0500

    day 99

---
+++ Take2/Day_099_SwitchIT.js
@@ -0,0 +1,11 @@
+function switchItUp(number){
+    return (number === 1 ? 'One':
+           (number === 2 ? 'Two':
+           (number === 3 ? 'Three':
+           (number === 4 ? 'Four':
+           (number === 5 ? 'Five':
+           (number === 6 ? 'Six':
+           (number === 7 ? 'Seven':
+           (number === 8 ? 'Eight':
+           (number === 9 ? 'Nine': 'Zero')))))))))
+  }
