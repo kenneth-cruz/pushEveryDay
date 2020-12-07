f830f35c0a200241939cc49a45e0e974dd1c380d
kenneth-cruz <kenneth.s.cruz@gmail.com>
2020-12-07 07:57:24 -0500

    day 113

---
+++ Take2/Day_113_numberPythonBugs.py
@@ -0,0 +1,8 @@
+def what_is(x):
+    print(x)
+    if x is int(42):
+        return 'everything'
+    elif (x == int(1764)):
+        return 'everything squared'
+    else:
+        return 'nothing'
