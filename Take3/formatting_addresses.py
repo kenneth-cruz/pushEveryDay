8486278888f05e18255dc14f52fe07dea306caa8
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2021-04-18 10:46:57 -0400

    formatting address

---
+++ Take3/formatting_addresses.py
@@ -0,0 +1,13 @@
+def format_address(address_string):
+  house_number = address_string.split(' ')[0]
+  street_name = ' '.join(address_string.split(' ')[1:])
+  return "house number {} on street named {}".format(house_number, street_name)
+
+print(format_address("123 Main Street"))
+# Should print: "house number 123 on street named Main Street"
+
+print(format_address("1001 1st Ave"))
+# Should print: "house number 1001 on street named 1st Ave"
+
+print(format_address("55 North Center Drive"))
+# Should print "house number 55 on street named North Center Drive"
