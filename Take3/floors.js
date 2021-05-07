ab0bca4dfae3bb4352e3e4f73b6b2c2adfdfdf2d
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2021-05-07 17:33:17 -0400

    floors

---
+++ Take3/floors.js
@@ -0,0 +1,14 @@
+function getRealFloor(n) {
+    console.log(n)
+    if (n === 0){
+      return 0
+    }
+    if (n < 13 && n > 0){
+      return  n -1
+    }
+    if (n < 0){
+      return n 
+    }
+    else{
+      return n -2
+    }
